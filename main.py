0dc20d28567e8f6d948ac55dd3aa182ba5017f56
AviAvinav <54372449+AviAvinav@users.noreply.github.com>
2020-06-13 08:41:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sb
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv('kc_house_data.csv')
+

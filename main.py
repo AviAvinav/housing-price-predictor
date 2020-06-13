41214f9a8954947c6540e028e8b093dcfcbe61b2
AviAvinav <54372449+AviAvinav@users.noreply.github.com>
2020-06-14 00:17:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv('kc_house_data.csv')
 
+data.info()
+data.head()
+
+correlation = data.corr(method = 'pearson')
+plt.figure(figsie(21,21))
+sb.heatmap(correlation, xticklabaels=correlation.columns, yticklabels=correlation.columns, annot=True, linewidth=1)
+
+training_data, test_data = train_test_split(data, train_size=0.8, random_state=0)
+regress = LinearRegression()
+
+x_train = np.array()
